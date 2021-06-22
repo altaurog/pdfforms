5e5e45ddc7c4de5802baff0b6f0106f8f0b6d417
Aryeh Leib Taurog <git@aryehleib.com>
2021-06-22 23:13:20 +0300

    chore: add python versions

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
