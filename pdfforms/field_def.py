f3c7c29f26ce896aeafa8dde2c78e8f395a5c1fb
Karsten Pufahl <kontakt@karstenpufahl.de>
2022-02-04 17:53:29 +0100

    utf8-support

--- pdfforms/field_def.py
+++ pdfforms/field_def.py
@@ -5,13 +5,13 @@ import json
 def save_field_defs(field_defs_file, field_defs):
     "save field definitions"
     with open(field_defs_file, "w") as f:
-        json.dump(field_defs, f, indent=4)
+        json.dump(field_defs, f, indent=4,ensure_ascii=False)
 
 
 def load_field_defs(field_defs_file, fail_silently=False):
     "load field definitions"
     try:
-        with open(field_defs_file) as f:
+        with open(field_defs_file, encoding='utf-8') as f:
             return json.load(f)
     except OSError:
         if fail_silently:
